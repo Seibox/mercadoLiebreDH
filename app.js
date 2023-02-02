02121af1083c014e8cd7a3a8ed659bebf550178c
Seibox <seibomlove@gmail.com>
2023-02-02 09:33:37 -0300

    Primer commit, estructura html

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.listen(3000,function(){
+   console.log('Corriendo en el puerto 3000');
+});
+app.use(express.static('public'));
+app.get('/',function(req,res){
+    const rutaCompleta = path.join(__dirname,'/views/home.html');
+    res.sendFile(rutaCompleta);
+});
