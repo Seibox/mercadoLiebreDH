fdb49ee4511866df9da8999d673cbc8b96c6543a
Seibox <seibomlove@gmail.com>
2023-02-22 19:41:03 -0300

    modificación de package.json y app para render

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.listen(3000,function(){
-   console.log('Corriendo en el puerto 3000');
+   console.log('Corriendo en el puerto '+port);
 });
+const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.get('/',function(req,res){
     const rutaCompleta = path.join(__dirname,'/views/home.html');
